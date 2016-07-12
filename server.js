54ead71acfb5d122ce0f96838980e30ef11267b8
Tet Contado <delrio.tet@gmail.com>
2016-07-12 16:24:34 -0700

    Add @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,33 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// Sends current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	// prevent from searching clientInfo objects for rooms that don't exist
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	//search thru clientInfo object
+	//keys() - it takes an object and returns an array of  all the attributes on that object
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '), // join() - takes every element in an array, converts into string and pushes altogether
+		timestamp: moment().valueOf() 
+	});
+}
+
 //method - on(name_of_event, function to run when the event happens) - listen for events
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
@@ -46,10 +73,17 @@ io.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
-		// io.emit --> sends to every single person including the sender
-		//socket.broadcast.emit('message', message);  // sends to everyone but the sender
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message); 
+		//checks the value of message if it is equal to current users
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			// io.emit --> sends to every single person including the sender
+			//socket.broadcast.emit('message', message);  // sends to everyone but the sender
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message); 
+		}
+
+		
 	});
 
 	//emit function takes 2 args (event-name, data-to-send)
