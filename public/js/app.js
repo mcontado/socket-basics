8c9882889432b6dce0f2255c952044e4f450b72c
Tet Contado <delrio.tet@gmail.com>
2016-07-12 12:07:52 -0700

    Add front end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,26 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
 });
+
+socket.on('message', function (message) {
+	console.log('New messsage: ');
+	console.log(message.text);
+});
+
+// Handles submitting of new message 
+var $form = jQuery('#message-form');
+
+//submit event for submitting the form
+$form.on('submit', function (event) {
+	//preventDefault -  is used on form when you dont want to submit the old fashion way by refreshing entire page
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+	
+});
--- server.js
+++ server.js
@@ -7,8 +7,21 @@ var io = require('socket.io')(http);  //format that socket-io expects
 app.use(express.static(__dirname + '/public'));
 
 //method - on(name_of_event, function to run when the event happens) - listen for events
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+
+		// io.emit --> sends to every single person including the sender
+		socket.broadcast.emit('message', message);  // sends to everyone but the sender
+	});
+
+	//emit function takes 2 args (event-name, data-to-send)
+	//when the user successfully connects to the chat application
+	socket.emit('message', {
+		text: 'Welcome to the chat application!'
+	});
 });
 
 http.listen(PORT, function () {
